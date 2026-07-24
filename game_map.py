38bd4d9aa3d21e3a66bd771198d820e204527dc0
Masi-beep <maximo.lizarraga@gmail.com>
2026-07-24 14:04:13 -0300

    added the loop entity print to gamemap

--- game_map.py
+++ game_map.py
@@ -48,3 +48,13 @@ class GameMap:
                 choicelist=[self.tiles["light"], self.tiles["dark"]],
                 default=tile_types.SHROUD
         )
+
+        for entity in self.entities:
+            # only print entities that are in the FOV
+            if self.visible[entity.x, entity.y]:
+                console.print(
+                        x=entity.x, 
+                        y=entity.y, 
+                        string=entity.char,
+                        fg=entity.color
+                )
