9715aa8132907a7a7e98a42c60275a546cad29b1
Masi-beep <maximo.lizarraga@gmail.com>
2026-07-24 15:45:32 -0300

    well not really, that func was to check if there was an entity, now in action's there's the blocking!

--- actions.py
+++ actions.py
@@ -21,16 +21,22 @@ class Action:
         raise NotImplementedError()
 
 class EscapeAction(Action):
-    def perform(self, engine: Engine, entity: Entity) -> None:
+    def perform(self, engine:Engine, entity:Entity) -> None:
         raise SystemExit()
 
-class MovementAction(Action):
-    def __init__(self, dx: int, dy: int):
+class ActionWithDirection(Action):
+    def __init__(self, dx:int, dy:int):
         super().__init__()
+
         self.dx = dx
         self.dy = dy
 
-    def perform(self, engine: Engine, entity: Entity) -> None:
+    def perform(self, engine:Engine, entity:Entity) -> None:
+        raise NotImplementedError()
+
+class MovementAction(ActionWithDirection):
+
+    def perform(self, engine:Engine, entity:Entity) -> None:
         dest_x = entity.x + self.dx
         dest_y = entity.y + self.dy
 
@@ -38,5 +44,7 @@ class MovementAction(Action):
             return # destination is out of bounds
         if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
             return # destination blocked by tile
+        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+            return # destination blocked by an entity
 
         entity.move(self.dx, self.dy)
