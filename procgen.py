66f66a45bebdf1292da8db9279a8565463682095
Masi-beep <maximo.lizarraga@gmail.com>
2026-07-24 14:27:26 -0300

    created the place entities func, but it doesnt place entities yet...

--- procgen.py
+++ procgen.py
@@ -42,6 +42,21 @@ class RectangularRoom:
             and self.y2 >= other.y1
         )
 
+def place_entities(
+    room: RectangularRoom, dungeon: GameMap, maximum_monsters:int,
+) -> None:
+    number_of_monsters = random.randint(0, maximum_monsters)
+    
+    for i in range(number_of_monsters):
+        x = random.randint(room.x1 + 1, room.x2 - 1)
+        y = random.randint(room.y1 + 1, room.y2 - 1)
+        
+        if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
+            if random.random() <0.8:
+                pass # TODO: Place orc here
+            else:
+                pass # place troll here
+
 def tunnel_between(
         start: Tuple[int, int], end: Tuple[int, int]
 ) -> Iterator[Tuple[int, int]]:
@@ -101,6 +116,8 @@ def generate_dungeon(
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
                 dungeon.tiles[x, y] = tile_types.floor
         
+        place_entities(new_room, dungeon, max_monsters_per_room)
+
         # finally append the new room to the list
         rooms.append(new_room)
     
