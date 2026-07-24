9a1b174c2403a9e237c50cb844a3540725791690
Masi-beep <maximo.lizarraga@gmail.com>
2026-07-24 12:08:24 -0300

    finished part 4 FOV

--- game_map.py
+++ game_map.py
@@ -11,11 +11,32 @@ class GameMap:
         self.tiles = np.full(
                 (width, height), 
                 fill_value=tile_types.wall, 
-                order="F")
-
+                order="F"
+        )
+        self.visible = np.full(
+                (width, height),
+                fill_value=False,
+                order="F"
+        )
+        self.explored = np.full(
+                (width, height),
+                fill_value=False,
+                order="F"
+        )
     def in_bounds(self, x: int, y: int) -> bool:
         # return true if x and y are inside the bounds of this map
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
-        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        """
+        Renders the map.
+
+        If a tile is in 'visible' array, then draw it with the light color
+        If it isnt, but is in the 'explored array, draw it with dark color
+        otherwise, default is 'SHROUD'
+        """
+        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+                condlist=[self.visible, self.explored],
+                choicelist=[self.tiles["light"], self.tiles["dark"]],
+                default=tile_types.SHROUD
+        )
