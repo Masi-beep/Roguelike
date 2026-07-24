4db4147a9a39c23460e32547db86896850671867
Masi-beep <maximo.lizarraga@gmail.com>
2026-07-24 15:14:08 -0300

    player now is copied instead of created inside main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # main is reponsible for running the main loop and handling the logic.
+import copy
+
 import tcod
 
 from engine import Engine
-from entity import Entity
+import entity_factories
 from input_handlers import EventHandler
 from procgen import generate_dungeon
 
@@ -26,28 +28,23 @@ def main() -> None:
     
     event_handler = EventHandler()
 
-    player = Entity(
-            int(screen_width / 2), 
-            int(screen_height / 2), 
-            '@', 
-            (255, 255, 255)
-            )
+    player = copy.deepcopy(entity_factories.player)
     
     game_map = generate_dungeon(
-            max_rooms=max_rooms,
-            room_min_size=room_min_size,
-            room_max_size=room_max_size,
-            map_width=map_width,
-            map_height=map_height,
-            max_monsters_per_room=max_monsters_per_room,
-            player=player
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        max_monsters_per_room=max_monsters_per_room,
+        player=player
     )
 
     engine = Engine(
-                    event_handler=event_handler,
-                    game_map=game_map,
-                    player=player
-                    )
+        event_handler=event_handler,
+        game_map=game_map,
+        player=player
+    )
 
     with tcod.context.new_terminal(
         screen_width,
@@ -58,8 +55,7 @@ def main() -> None:
     ) as context:
         root_console = tcod.console.Console(screen_width, 
                                             screen_height, 
-                                            order="F"
-                                            )
+                                            order="F")
         while True:
             engine.render(console=root_console, context=context)
             
