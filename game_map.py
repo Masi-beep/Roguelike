0bba3d19a5056bfcce9020d1c73afdc0be9372a8
Masi-beep <maximo.lizarraga@gmail.com>
2026-07-24 15:38:41 -0300

    added the can't go through entities function

--- game_map.py
+++ game_map.py
@@ -1,7 +1,7 @@
 # game map
 from __future__ import annotations
 
-from typing import Iterable, TYPE_CHECKING
+from typing import Iterable, Optional, TYPE_CHECKING
 
 import numpy as np # type ignore
 from tcod.console import Console
@@ -36,6 +36,18 @@ class GameMap:
                 fill_value=False,
                 order="F"
         )
+    
+    def get_blocking_entity_at_location(
+        self, 
+        location_x: int, 
+        location_y: int,
+    ) -> Optional[Entity]:
+        for entity in self.entities:
+            if entity.blocks_movement and entity.x == location_x and entity.y == location_y:
+                return entity
+        
+        return None
+
     def in_bounds(self, x: int, y: int) -> bool:
         # return true if x and y are inside the bounds of this map
         return 0 <= x < self.width and 0 <= y < self.height
