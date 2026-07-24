0b2fb503f5043af4cec6707683a2de6a62290d8e
Masi-beep <maximo.lizarraga@gmail.com>
2026-07-24 14:17:50 -0300

    added max enemies var to gen dungeon

--- procgen.py
+++ procgen.py
@@ -67,6 +67,7 @@ def generate_dungeon(
         room_max_size: int,
         map_width: int, 
         map_height: int,
+        max_monsters_per_room: int,
         player: Entity
 ) -> GameMap:
     # generate a new dungeon map
