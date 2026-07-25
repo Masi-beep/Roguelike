4095c05e03182b74997d9df641eb671025503396
Masi-beep <maximo.lizarraga@gmail.com>
2026-07-24 21:16:45 -0300

    entity now refrences gamemap

--- entity.py
+++ entity.py
@@ -1,7 +1,7 @@
 # entity class eoeoeo
 import copy
 
-from typing import Tuple, TypeVar, TYPE_CHECKING
+from typing import Optional, Tuple, TypeVar, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from game_map import GameMap
@@ -11,8 +11,12 @@ T = TypeVar("T", bound="Entity")
 
 class Entity:
     # generic object to represent players, enemies, items, etc.
+
+    gamemap: GameMap
+
     def __init__(
-        self, 
+        self,
+        gamemap: Optional[GameMap] = None,
         x:int = 0, 
         y:int = 0, 
         char:str = "?", 
@@ -26,14 +30,34 @@ class Entity:
         self.color = color
         self.name = name 
         self.blocks_movement = blocks_movement
+        if gamemap:
+            # if its not provided now then it will be set later
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
 
     def spawn(self: T, gamemap: GameMap, x: int, y: int) -> T:
         # spawn a copy of this instance at the given location
         clone = copy.deepcopy(self)
         clone.x = x
         clone.y = y
+        clone.gamemap = gamemap
         gamemap.entities.add(clone)
         return clone
+    
+    def place(
+        self,
+        x: int,
+        y: int,
+        gamemap: Optional[GameMap] = None
+    ) -> None:
+        # place this entity at a new location. handles moving across GameMap
+        self.x = x
+        self.y = y
+        if gamemap:
+            if hasattr(self, "gamemap"): # possibly unintialized
+                self.gamemap.entities.remove(self)
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
 
     def move(self, dx:int, dy:int) -> None:
         # Move entity by given amount
